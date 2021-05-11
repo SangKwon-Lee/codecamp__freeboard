19a9650c5ba7c9623f1d8725d25b821475d16174
이상권 <sk@isang-gwon-ui-MacBookPro.local>
2021-05-11 10:53:45 +0900

    TEST

--- pages/board/index.js
+++ pages/board/index.js
@@ -37,7 +37,7 @@ export default function BoardWrithePage() {
 		<Container>
 			<BoardWrapper>
 				<TitleWrapper>
-					<Title>게시물 등록</Title>
+					<Title>게시물 등록aa</Title>
 				</TitleWrapper>
 
 				<NameAndPassword>
